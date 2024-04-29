db412af8fe0463237524f04e737d6f7470123dfc
JP Barba <jpbs92@hotmail.com>
2024-04-29 14:51:49 +0000

    feat: add missing platforms where possible

--- src/getDockerImagePlatforms.mjs
+++ src/getDockerImagePlatforms.mjs
@@ -2,16 +2,18 @@ import { exec } from "child_process";
 
 export async function getDockerImagePlatforms(dockerImage) {
   const inspect = await getDockerInspect(dockerImage);
-  console.dir(inspect.manifests);
+  const isAlpine = dockerImage.includes('alpine')
   return inspect.manifests.reduce((platforms, manifest) => {
     const platform = `${manifest.platform.os}/${manifest.platform.architecture}`;
-    if (["amd64", "arm64"].includes(manifest.platform.architecture)) {
-      platforms.push(
-        manifest.platform.variant
-          ? `${platform}/${manifest.platform.variant}`
-          : platform
-      );
+    if (isAlpine && !["amd64", "arm64"].includes(manifest.platform.architecture)) {
+      return platforms
     }
+
+    platforms.push(
+      manifest.platform.variant
+        ? `${platform}/${manifest.platform.variant}`
+        : platform
+    );
     return platforms;
   }, []);
 }
